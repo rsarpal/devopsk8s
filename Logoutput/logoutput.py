c727e258ed27416ff5a06f4da68066314e3d12c6
Rishu <sarpal.rishu@gmail.com>
2021-11-14 20:29:44 +0200

    Exercise 1.11: Persisting data 1. Persistent volumes added 2. Logoutput updated to use persistent volumes and to output pingpong counts 3. Pingpong updated to use persistent volumes

--- Logoutput/logoutput.py
+++ Logoutput/logoutput.py
@@ -1,8 +1,26 @@
 #!/Users/admin/anaconda/bin/python3
 import uuid
 import datetime
-import time
+from time import sleep
+import os.path
+from http.server import HTTPServer, BaseHTTPRequestHandler 
 
-while True:
-    print (f"<p>{datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')}  {str(uuid.uuid4())}</p>")
-    time.sleep(5)
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler): 
+
+
+  def readFile(self):
+
+    while not os.path.exists("/tmp/kube/ping.txt"):
+      sleep(5)
+    
+    with open("/tmp/kube/ping.txt","r") as f:
+       return(f.readline())
+
+  def do_GET(self):
+    self.send_response(200) 
+    self.end_headers() 
+    line= f"<p>{datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')}  {str(uuid.uuid4())}</p></br>" + self.readFile()
+    self.wfile.write(line.encode("utf-8"))
+
+httpd = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
--- Logoutput/startup.sh
+++ Logoutput/startup.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "START Running Django on `date`"
-python3 -u logoutput.py >> index.html &
-python3 -m http.server 8000
+python3 logoutput.py #>> index.html &
+#python3 -m http.server 8000
